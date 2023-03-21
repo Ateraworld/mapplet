5c9050bb41cfea66f094533ea4e6088b57e786c3
Matteo Beltrame <bltmtt@gmail.com>
2023-03-21 18:39:47 +0100

    Update mapplet.dart

--- lib/mapplet.dart
+++ lib/mapplet.dart
@@ -1,8 +1,22 @@
 library mapplet;
 
 import "dart:async";
+
 import "package:mapplet/src/depot/depot.dart";
 import "package:mapplet/src/depot/depot_config.dart";
+
+export "package:mapplet/src/common/extensions.dart";
+
+export "package:mapplet/src/database/depot_database.dart";
+export "package:mapplet/src/database/depot_stats.dart";
+export "package:mapplet/src/database/models/region_model.dart";
+export "package:mapplet/src/database/models/tile_model.dart";
+
+export "package:mapplet/src/depot/depot.dart";
+export "package:mapplet/src/depot/depot_config.dart";
+export "package:mapplet/src/depot/fetch_operation.dart";
+
+export "package:mapplet/src/providers/map_tile_provider.dart";
 
 /// Entry point for handling offline maps with OffMap
 class Mapplet {
